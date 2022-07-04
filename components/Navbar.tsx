a7ce95461c42ef74d37a2a3128f74c89fff083c3
Nazarii <nazariikubik@gmail.com>
2022-07-04 10:47:43 +0200

    add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+import styles from "../styles/Navbar.module.scss";
+import { motion } from "framer-motion";
+
+export const Navbar = (props: any) => {
+  const variants = {
+    open: { opacity: 1, x: "100%" },
+    closed: { opacity: 1, x: 30 },
+  };
+  return (
+    <>
+      <Head>
+        <title>Nazarii Kubik</title>
+      </Head>
+      <div>
+        <div className={styles.navbar}>
+          <div
+            className={styles.btnClose}
+            onClick={() => props.setOpen(!props.open)}
+          >
+            {props.open ? (
+              <Image
+                alt="closebtn"
+                src={"/btnOpen.svg"}
+                width={30}
+                height={30}
+              />
+            ) : (
+              <Image
+                alt="closebtn"
+                src={"/btnClose.svg"}
+                width={30}
+                height={30}
+              />
+            )}
+          </div>
+
+          <motion.ul
+            animate={props.open ? "open" : "closed"}
+            variants={variants}
+            className={styles.navbar__wrapper}
+          >
+            <li>About Me</li>
+            <li>My Stack</li>
+            <li>Projects</li>
+            <li>Contacts</li>
+          </motion.ul>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {" "}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Archivo:wght@600&family=Open+Sans&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
