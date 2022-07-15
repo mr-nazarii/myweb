1abf9226c4411f1cbc7d35ce25ad2a6774299fc2
Nazarii <nazariikubik@gmail.com>
2022-07-15 14:18:15 +0200

    add disable button

--- components/Btn/BtnVariants.ts
+++ components/Btn/BtnVariants.ts
@@ -39,3 +39,24 @@ export const buttonVariantsBlack = {
     paddingLeft: "5px",
   },
 };
+
+export const buttonVariantsExit = {
+  hover: {
+    backgroundColor: "#d73d4d",
+    color: "#b62231",
+    borderBottom: "6px #e48080 solid",
+    borderLeft: "4px #f8a8a8 solid",
+    boxShadow: "inset 5px -5px 20px -6px #ffffff0",
+  },
+
+  click: {
+    boxShadow: "inset 5px -5px 20px -6px #00000087",
+    borderBottom: "0px #e48080 solid",
+    borderLeft: "0px #f8a8a8 solid",
+    backgroundColor: "#66121a",
+    color: "#604b00",
+    borderTop: "4px #462626 solid",
+    borderRight: "8px #552828 solid",
+    paddingLeft: "5px",
+  },
+};
--- components/Shield/Shield.tsx
+++ components/Shield/Shield.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import {
+  buttonVariantsBlack,
+  buttonVariantsExit,
+} from "components/Btn/BtnVariants";
 
 export const Shield = (props: any) => {
   const shieldVariant = {
@@ -11,6 +15,25 @@ export const Shield = (props: any) => {
       x: 0,
     },
   };
+
+  const activeVariant = {
+    closed: {
+      color: "#961a27",
+      background: "linear-gradient(90deg, #d92638 0%, #c76666 100%)",
+      transition: {
+        delay: 1.5,
+      },
+    },
+    open: {
+      color: "#1a962f",
+      background: "linear-gradient(90deg, #3bd926 0%, #68c766 100%)",
+
+      transition: {
+        delay: 1.5,
+      },
+    },
+  };
+
   return (
     <>
       <motion.div
@@ -23,6 +46,26 @@ export const Shield = (props: any) => {
         <div className={props.styles.shield__top}>
           <div className={props.styles.shield__danger} />
           <p>Defence Line</p>
+          <div className={props.styles.actionWrapper}>
+            <motion.p
+              variants={activeVariant}
+              initial={"closed"}
+              animate={props.active ? "open" : "closed"}
+              className={props.styles.actionWrapper__activeLamp}
+            >
+              active
+            </motion.p>
+
+            <motion.div
+              variants={buttonVariantsExit}
+              whileHover={"hover"}
+              whileTap={"click"}
+              className={props.styles.link}
+              onClick={() => props.setActive(!props.active)}
+            >
+              <Image alt="phone" width={20} height={20} src="/signOut.svg" />
+            </motion.div>
+          </div>
         </div>
         <div className={props.styles.shield__glass}></div>
       </motion.div>
