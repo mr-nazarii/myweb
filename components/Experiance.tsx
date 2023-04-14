2fdecf44244a8ba5489894e1dc4e283d639a415e
Nazarii <nazariikubik@gmail.com>
2023-04-13 23:13:43 -0400

    add new image

--- components/Experiance.tsx
+++ components/Experiance.tsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Btn } from "./Btn/Btn";
 import styles from "../styles/Experiance.module.scss";
 
 export const Experiance = () => {
   const [active, setActive] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = ref.current?.scrollTop;
+      const totalHeight = ref.current?.scrollHeight - ref.current?.clientHeight;
+      const progress = (scrollPosition / totalHeight) * 100;
+      setScrollProgress(totalHeight);
+      console.log(progress);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
       <div
         className={styles.section}
+        ref={ref}
         style={{
           backgroundImage: "url('./frame2.png')",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
+          position: "relative",
+          height: "800px",
         }}
         id="aboutMe"
       >
@@ -22,10 +40,9 @@ export const Experiance = () => {
 
         <div
           style={{
-            position: "sticky",
             top: "150px",
             width: "10px",
-            height: "10px",
+            height: `${scrollProgress}%`,
             background: "yellow",
           }}
         ></div>
