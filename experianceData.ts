3f7b34d52773107ebc8851afbefc127a50a6b68b
Nazarii <nazariikubik@gmail.com>
2023-05-26 20:33:42 -0400

    Update Info

--- experianceData.ts
+++ experianceData.ts
@@ -1,5 +1,17 @@
 export const getExperianceData = () => [
   {
+    company: "Law firm of 'Bogdan Kubik'",
+    position: "Chief Design Officer",
+    employmentPeriod: {
+      start: new Date(2018, 10, 1),
+      end: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+    },
+    location: "Toronto, Ontario, Canada",
+    responsibilities: [
+      "Established design guidelines to ensure brand consistency across all digital and print media. I worked closely with our marketing team to ensure that our brand identity is accurately represented and resonates with our target audience. Led user research initiatives to gain insights into our clients' needs and preferences. Implemented key accessibility features across our digital platforms, ensuring compliance with WCAG standards. Leveraged analytics tools to assess the performance of our digital platforms, identifying areas for improvement and measuring the success of various design initiatives. This approach has allowed us to continually refine our platforms and services.",
+    ],
+  },
+  {
     company: "Brand Vision Marketing",
     position: "UX/UI Designer & Front-End Developer",
     employmentPeriod: {
@@ -13,10 +25,10 @@ export const getExperianceData = () => [
   },
   {
     company: "Unicorn",
-    position: "Full Stack Developer Intern",
+    position: "Full Stack Developer",
     employmentPeriod: {
       start: new Date(2022, 1, 1),
-      end: new Date(2022, 2, 1),
+      end: new Date(2022, 11, 1),
     },
     location: "Frankfurt, Hesse, Germany",
     responsibilities: [
@@ -27,12 +39,36 @@ export const getExperianceData = () => [
     company: "DAN-IT Education",
     position: "Full-Stack Development Student",
     employmentPeriod: {
-      start: new Date(2020, 8, 1),
-      end: new Date(2023, 1, 1),
+      start: new Date(2020, 7, 1),
+      end: new Date(2023, 0, 1),
     },
     location: "Toronto, Ontario, Canada",
     responsibilities: [
       "During my studies, I honed my JavaScript, CSS, and HTML skills and integrated UX/UI principles to develop dynamic, user-centric websites and React.js applications. I utilized Node.js for back-end development, ensuring seamless connectivity for front-end interfaces. I became proficient in CI/CD systems, Git operations, and automated Jest testing, facilitating high code quality and dependable deployments. My education provided a collaborative environment where I applied problem-solving skills to complex system issues, effectively enhancing system efficiency and user experience.",
+    ],
+  },
+  {
+    company: "Utex Ukraine LLC",
+    position: "UX/UI Designer",
+    employmentPeriod: {
+      start: new Date(2017, 0, 1),
+      end: new Date(2018, 10, 1),
+    },
+    location: "Kyiv, Ukraine",
+    responsibilities: [
+      "Took charge of the UX/UI design for a significant new product, starting from initial wireframes and prototypes, through to the final UI design. The product was well-received by users and stakeholders alike, contributing to a successful launch. Conducted user interviews and usability testing sessions for various products, providing valuable insights that informed design improvements. The enhancements improved the overall user experience and increased user engagement. Fostered a productive working relationship with the development team, ensuring smooth handoffs and effective implementation of designs. This cross-functional collaboration led to fewer revisions and a faster product development cycle.",
+    ],
+  },
+  {
+    company: "Utex Ukraine LLC",
+    position: "Junior UX/UI Designer",
+    employmentPeriod: {
+      start: new Date(2015, 2, 1),
+      end: new Date(2017, 0, 1),
+    },
+    location: "Kyiv, Ukraine",
+    responsibilities: [
+      "Collaborated with the senior UX/UI Designers and Product Managers to understand project specifications and user requirements, particularly as they related to cable manufacturing and fiber optic technologies. Assisted in designing and prototyping the user interface for the company's internal systems and software, focusing on ease-of-use, efficiency, and error reduction. Helped design UX/UI for applications used by the production line operators, improving their efficiency and reducing potential for errors. Participated in the creation of user personas and user journeys, specifically considering the unique needs and characteristics of users in the cable manufacturing industry.",
     ],
   },
 ];
