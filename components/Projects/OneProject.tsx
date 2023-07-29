9f804907d66d9f2e8450609843e25670a1d6cdfd
Nazarii <nazariikubik@gmail.com>
2023-07-29 02:38:45 -0400

    updated

--- components/Projects/OneProject.tsx
+++ components/Projects/OneProject.tsx
@@ -20,34 +20,25 @@ export const OneProject: React.FC<Props> = ({
 }) => {
   return (
     <div className={styles.project} style={{ background: `${color}` }}>
-      {/* <div className={styles.project__img}>
-        <Image
-          fill={true}
-          objectFit="cover"
-          alt={name}
-          src={`/projects/${name}/1.png`}
-          style={{ borderRadius: "35px" }}
-        />
-      </div> */}
-
-      <div className={styles.details}>
-        <h5 className={styles.details__name}>{desc}</h5>
-        <h5 className={styles.details__quote}>{quote}</h5>
-        <Link className={styles.details__action} key={id} href={`/${id}`}>
-          Learn More
-          <Image
-            width={40}
-            height={40}
-            alt={name}
-            src={`/icons/arrowRight.svg`}
-            style={{ borderRadius: "35px" }}
-          />
-        </Link>
+      <div className={styles.innerWrapper}>
+        <div className={styles.details}>
+          <h5 className={styles.details__name}>{desc}</h5>
+          <h5 className={styles.details__quote}>{quote}</h5>
+          <Link className={styles.details__action} key={id} href={`/${id}`}>
+            Learn More
+            <Image
+              width={40}
+              height={40}
+              alt={name}
+              src={`/icons/arrowRight.svg`}
+              style={{ borderRadius: "35px" }}
+            />
+          </Link>
+        </div>
+        <div className={styles.project__img}>
+          <Image alt={name} fill={true} src={`/projects/${name}/2.png?v=13`} />
+        </div>
       </div>
-      <div className={styles.project__img}>
-        <Image alt={name} fill={true} src={`/projects/${name}/2.png?v=13`} />
-      </div>
-
       {/*
       <div className={styles.details}>
         <div className={styles.detailsInner}>
--- components/TitleSection.tsx
+++ components/TitleSection.tsx
@@ -10,81 +10,83 @@ export const TitleSection = () => {
     <AnimatePresence exitBeforeEnter>
       <div className={styles.section}>
         <div className={styles.section__wrapper}>
-          <div className={styles.titleSection}>
-            <div className={styles.titleSection__title1}>
-              <h1>Welcome!</h1>
-              <div className={styles.handstyle}>
-                <Image
-                  alt="graph1"
-                  src={"/graphs/hand.png"}
-                  objectFit="contain"
-                  style={{ zIndex: 1 }}
-                  layout="fill"
-                />
+          <div className={styles.section__innerWrapper}>
+            <div className={styles.titleSection}>
+              <div className={styles.titleSection__title1}>
+                <h1>Welcome!</h1>
+                <div className={styles.handstyle}>
+                  <Image
+                    alt="graph1"
+                    src={"/graphs/hand.png"}
+                    objectFit="contain"
+                    style={{ zIndex: 1 }}
+                    layout="fill"
+                  />
+                </div>
+              </div>
+              <h1 className={styles.titleSection__title2}>I&#39;m Nazarii</h1>
+              <div className={styles.titleSection__title3}>
+                <h1>a</h1>
+                <h1>
+                  <ReactRotatingText
+                    items={[
+                      "UX/UI Designer",
+                      "Front-End Developer",
+                      "Entrepreneur",
+                    ]}
+                  />
+                </h1>
               </div>
             </div>
-            <h1 className={styles.titleSection__title2}>I&#39;m Nazarii</h1>
-            <div className={styles.titleSection__title3}>
-              <h1>a</h1>
-              <h1>
-                <ReactRotatingText
-                  items={[
-                    "UX/UI Designer",
-                    "Front-End Developer",
-                    "Entrepreneur",
-                  ]}
-                />
-              </h1>
+            <video className={styles.section__video} autoPlay loop muted>
+              <source src="/me.mp4" />
+            </video>
+            <div className={styles.graph1}>
+              <Image
+                alt="graph1"
+                src={"/graphs/1.png"}
+                objectFit="contain"
+                style={{ zIndex: 1 }}
+                layout="fill"
+              />
             </div>
-          </div>
-          <video className={styles.section__video} autoPlay loop muted>
-            <source src="/me.mp4" />
-          </video>
-          <div className={styles.graph1}>
-            <Image
-              alt="graph1"
-              src={"/graphs/1.png"}
-              objectFit="contain"
-              style={{ zIndex: 1 }}
-              layout="fill"
-            />
-          </div>
-          <div className={styles.graph2}>
-            <Image
-              alt="graph1"
-              src={"/graphs/2.png"}
-              objectFit="contain"
-              style={{ zIndex: 1 }}
-              layout="fill"
-            />
-          </div>
-          <div className={styles.graph3}>
-            <Image
-              alt="graph1"
-              src={"/graphs/3.png"}
-              objectFit="contain"
-              style={{ zIndex: 1 }}
-              layout="fill"
-            />
-          </div>
-          <div className={styles.graph4}>
-            <Image
-              alt="graph1"
-              src={"/graphs/4.png"}
-              objectFit="contain"
-              style={{ zIndex: 1 }}
-              layout="fill"
-            />
-          </div>
+            <div className={styles.graph2}>
+              <Image
+                alt="graph1"
+                src={"/graphs/2.png"}
+                objectFit="contain"
+                style={{ zIndex: 1 }}
+                layout="fill"
+              />
+            </div>
+            <div className={styles.graph3}>
+              <Image
+                alt="graph1"
+                src={"/graphs/3.png"}
+                objectFit="contain"
+                style={{ zIndex: 1 }}
+                layout="fill"
+              />
+            </div>
+            <div className={styles.graph4}>
+              <Image
+                alt="graph1"
+                src={"/graphs/4.png"}
+                objectFit="contain"
+                style={{ zIndex: 1 }}
+                layout="fill"
+              />
+            </div>
 
-          <div className={styles.graph5}>
-            <Image
-              alt="graph1"
-              src={"/graphs/5.png"}
-              objectFit="contain"
-              style={{ zIndex: 1 }}
-              layout="fill"
-            />
+            <div className={styles.graph5}>
+              <Image
+                alt="graph1"
+                src={"/graphs/5.png"}
+                objectFit="contain"
+                style={{ zIndex: 1 }}
+                layout="fill"
+              />
+            </div>
           </div>
         </div>
       </div>
