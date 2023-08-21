9b3ca0f3314f497d9c444b461ea1feababf4b072
Nazarii <nazariikubik@gmail.com>
2023-08-21 13:09:16 -0700

    fix change

--- components/Projects/Ka.tsx
+++ components/Projects/Ka.tsx
@@ -365,27 +365,6 @@ export const Kaleidoscope = (project) => {
           </div>
         </div>
       </div>
-
-      <a
-        href="https://maxime-malfilatre.com"
-        className="signature"
-        target="_blank"
-      >
-        <div className="link">
-          <span className="brace"></span>
-          <span className="before">
-            <span className="before-text">&nbsp;by</span>
-          </span>
-          <span className="my-name">m</span>
-          <span className="after">
-            <span className="after-text">
-              <span className="spin">axe</span>
-              <span>w</span>
-            </span>
-          </span>
-          <span className="brace"></span>
-        </div>
-      </a>
     </div>
   );
 };
